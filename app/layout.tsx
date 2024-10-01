3417da02ef38ac4f8e57d25c90679ac944e86a34
Paul Geeser <commit@pauldev.sh>
2024-10-01 11:52:50 +0200

    :chart_with_upwards_trend: Added Analytics
    Added vercel analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -31,6 +32,7 @@ export default function RootLayout({
         {children}
         {/* ------------------------------- Bottom Blur ------------------------------ */}
         <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"/>
+        <Analytics/>
       </body>
     </html>
   );
