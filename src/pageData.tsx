1cde7716f2129e02acfdf4fad38f87727cef814b
Paul Geeser <commit@pauldev.sh>
2025-11-19 12:25:06 -0300

    📝 Updated PageData

--- src/pageData.tsx
+++ src/pageData.tsx
@@ -1,5 +1,6 @@
-import { type IconDefinition, faGithub, faLinkedinIn, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedinIn, faXTwitter, faEthereum } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 
 export const pageData = {
     pageTitle: "Paul Geeser | Software Engineer",
@@ -121,12 +122,21 @@ export const pageData = {
             title: "Slash Berlin",
             logo: "/hackathons/slashberlin.png",
             location: "Berlin, Germany",
+			category: "Sustainability",
             locationPoint: [52.510885, 13.3989367],
             livingLocationPoint: [49.1427, 9.2109],
             startDate: new Date("2022-09-23"),
             endDate: new Date("2022-09-25"),
-            description: "Built a mobile app similar to a Tamagotchi, which you can feed with the products you consume. Based on their impact on the environment, its healthy or not.",
-        },
+            description:
+                "Built a mobile app similar to a Tamagotchi, which you can feed with the products you consume. Based on their impact on the environment, its healthy or not.",
+			links: [
+				{
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/tamabara"
+				}
+			]
+		},
         {
             title: "ETHGlobal Lisbon",
             category: "Crypto/Blockchain",
@@ -136,6 +146,18 @@ export const pageData = {
             livingLocationPoint: [49.1427, 9.2109],
             startDate: new Date("2023-05-12"),
             endDate: new Date("2023-05-14"),
+			links: [
+				{
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/minimalens"
+				},
+				{
+					title: "ETHGlobal",
+					icon: faEthereum,
+					link: "https://ethglobal.com/showcase/minimalens-q4ivz"
+				}
+			]
         },
         {
             title: "ETHMunich",
@@ -144,14 +166,27 @@ export const pageData = {
             location: "Munich, Germany",
             locationPoint: [48.1371079, 11.5753822],
             livingLocationPoint: [49.1427, 9.2109],
-            description: "Developed a platform connecting individuals selling advertising space to businesses looking to advertise ",
+            description:
+                "Developed a platform connecting individuals selling advertising space to businesses looking to advertise ",
             startDate: new Date("2023-08-11"),
             endDate: new Date("2023-08-13"),
-			prizes: [
+            prizes: [
+                {
+                    title: "🥇 Best real world use case - 1st place",
+                },
+            ],
+			links: [
 				{
-					title: "🥇 Best real world use case - 1st place",
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/pauldev20/block-board"
 				},
-			],
+				{
+					title: "Showcase",
+					icon: faGlobe,
+					link: "https://devfolio.co/projects/blockboard-0366"
+				}
+			]
         },
         {
             title: "ETHGlobal New York",
@@ -163,14 +198,26 @@ export const pageData = {
             description: "Built a platform for leveraged perpetual futures in a never-before-seen way.",
             startDate: new Date("2023-09-22"),
             endDate: new Date("2023-09-24"),
-			prizes: [
+            prizes: [
+                {
+                    title: "🏆 UMA",
+                },
+                {
+                    title: "🏆 NEAR Protocol",
+                },
+            ],
+			links: [
 				{
-					title: "🏆 UMA",
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/pauldev20/pepetuals"
 				},
 				{
-					title: "🏆 NEAR Protocol",
-				},
-			],
+					title: "ETHGlobal",
+					icon: faEthereum,
+					link: "https://ethglobal.com/showcase/pepetuals-kyyw9"
+				}
+			]
         },
         {
             title: "ETHGlobal Istanbul",
@@ -179,23 +226,36 @@ export const pageData = {
             location: "Istanbul, Turkey",
             locationPoint: [41.006381, 28.9758715],
             livingLocationPoint: [49.1427, 9.2109],
-            description: "Developed a fully functioning browser-based wallet that completely abstracts the different chains, so its easy to use for the user.",
+            description:
+                "Developed a fully functioning browser-based wallet that completely abstracts the different chains, so its easy to use for the user.",
             startDate: new Date("2023-11-17"),
             endDate: new Date("2023-11-19"),
             prizes: [
                 {
                     title: "🥇 ETHGlobal Finalist",
                 },
-				{
-					title: "🥇 Best dApps on Celo Blockchain",
-				},
-				{
-					title: "🥈 Best Interchain Application",
-				},
-				{
-					title: "🥉 SAFE{CORE} ACCOUNT ABSTRACTION SDK",
-				}
+                {
+                    title: "🥇 Best dApps on Celo Blockchain",
+                },
+                {
+                    title: "🥈 Best Interchain Application",
+                },
+                {
+                    title: "🥉 SAFE{CORE} ACCOUNT ABSTRACTION SDK",
+                },
             ],
+			links: [
+				{
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/pauldev20/bridge-buddy"
+				},
+				{
+					title: "ETHGlobal",
+					icon: faEthereum,
+					link: "https://ethglobal.com/showcase/bridgebuddy-36te1"
+				}
+			]
         },
         {
             title: "ETHGlobal London",
@@ -204,17 +264,30 @@ export const pageData = {
             location: "London, UK",
             locationPoint: [51.5074456, -0.1277653],
             livingLocationPoint: [49.1427, 9.2109],
-            description: "Created a Farcaster Frame system for secure, anonymous polls using MACI and World ID. Reverse engineered the WorldID SDK to integrate it into a Farcaster Frame.",
+            description:
+                "Created a Farcaster Frame system for secure, anonymous polls using MACI and World ID. Reverse engineered the WorldID SDK to integrate it into a Farcaster Frame.",
             startDate: new Date("2024-03-15"),
             endDate: new Date("2024-03-17"),
-			prizes: [
+            prizes: [
+                {
+                    title: "🏆 Best use of MACI",
+                },
+                {
+                    title: "🥇 Best World ID Usage",
+                },
+            ],
+			links: [
 				{
-					title: "🏆 Best use of MACI",
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/pauldev20/votelik-pollerin"
 				},
 				{
-					title: "🥇 Best World ID Usage",
-				},
-			],
+					title: "ETHGlobal",
+					icon: faEthereum,
+					link: "https://ethglobal.com/showcase/votelik-pollerin-u0dcs"
+				}
+			]
         },
         {
             title: "ETHGlobal Brussels",
@@ -223,17 +296,30 @@ export const pageData = {
             location: "Brussels, Belgium",
             locationPoint: [50.8465573, 4.351697],
             livingLocationPoint: [49.1427, 9.2109],
-            description: "Built a betting platform designed for ETHGlobal attendees to predict hackathon finalists using showcase data, with seamless onboarding through NFC wristband sign-ins",
+            description:
+                "Built a betting platform designed for ETHGlobal attendees to predict hackathon finalists using showcase data, with seamless onboarding through NFC wristband sign-ins",
             startDate: new Date("2024-07-12"),
             endDate: new Date("2024-07-14"),
             prizes: [
                 {
                     title: "🥇 ETHGlobal Finalist",
                 },
-				{
-					title: "🏆 Honorable Mention",
-				}
+                {
+                    title: "🏆 Honorable Mention",
+                },
             ],
+			links: [
+				{
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/pauldev20/bananabets"
+				},
+				{
+					title: "ETHGlobal",
+					icon: faEthereum,
+					link: "https://ethglobal.com/showcase/bananabets-w0jbp"
+				}
+			]
         },
         {
             title: "ETHGlobal Singapore",
@@ -242,7 +328,8 @@ export const pageData = {
             location: "Singapore, Singapore",
             locationPoint: [1.357107, 103.8194992],
             livingLocationPoint: [49.1427, 9.2109],
-            description: "Developed a service which abstracts the need to manually transfer gas to each chain, this is done automatically by the service.",
+            description:
+                "Developed a service which abstracts the need to manually transfer gas to each chain, this is done automatically by the service.",
             startDate: new Date("2024-09-20"),
             endDate: new Date("2024-09-22"),
             prizes: [
@@ -256,6 +343,18 @@ export const pageData = {
                     title: "🥇 NounsDAO - Expand Public Goods",
                 },
             ],
+			links: [
+				{
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/pauldev20/fillmeup"
+				},
+				{
+					title: "ETHGlobal",
+					icon: faEthereum,
+					link: "https://ethglobal.com/showcase/fill-me-up-9u13c"
+				}
+			]
         },
         {
             title: "ETHGlobal San Francisco",
@@ -264,7 +363,8 @@ export const pageData = {
             location: "San Francisco, USA",
             locationPoint: [37.7792588, -122.4193286],
             livingLocationPoint: [49.1427, 9.2109],
-            description: "Created a platform for event organizers to let their customers trade beer. The more users purchase beer, the higher the prices rise.",
+            description:
+                "Created a platform for event organizers to let their customers trade beer. The more users purchase beer, the higher the prices rise.",
             startDate: new Date("2024-10-18"),
             endDate: new Date("2024-10-20"),
             prizes: [
@@ -273,6 +373,18 @@ export const pageData = {
                     link: "https://ens.domains",
                 },
             ],
+			links: [
+				{
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/pauldev20/beertalik-brokerin"
+				},
+				{
+					title: "ETHGlobal",
+					icon: faEthereum,
+					link: "https://ethglobal.com/showcase/beertalik-brokerin-57rkv"
+				}
+			]
         },
         {
             title: "ETHGlobal Bangkok",
@@ -281,7 +393,8 @@ export const pageData = {
             location: "Bangkok, Thailand",
             locationPoint: [13.7524938, 100.4935089],
             livingLocationPoint: [49.1427, 9.2109],
-            description: "Built a oracle platform similar to UMA, which utilizes WorldID to minimize the risk that the oracle is manipulated through stake-based governance.",
+            description:
+                "Built a oracle platform similar to UMA, which utilizes WorldID to minimize the risk that the oracle is manipulated through stake-based governance.",
             startDate: new Date("2024-11-15"),
             endDate: new Date("2024-11-17"),
             prizes: [
@@ -290,6 +403,18 @@ export const pageData = {
                     link: "https://world.org",
                 },
             ],
+			links: [
+				{
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/pauldev20/proofoftruth"
+				},
+				{
+					title: "ETHGlobal",
+					icon: faEthereum,
+					link: "https://ethglobal.com/showcase/proofoftruth-g31wn"
+				}
+			]
         },
         {
             title: "ETHGlobal Trifecta",
@@ -298,17 +423,30 @@ export const pageData = {
             location: "Online",
             locationPoint: [49.1427, 9.2109],
             livingLocationPoint: [49.1427, 9.2109],
-            description: "Developed a system that securly creates a verified assosiaction between passport data and a wallet, this is done by reading the passport datha through NFC (ICAO Doc 9303) and validating it inside a TEE, which then submits it onchain.",
+            description:
+                "Developed a system that securly creates a verified assosiaction between passport data and a wallet, this is done by reading the passport datha through NFC (ICAO Doc 9303) and validating it inside a TEE, which then submits it onchain.",
             startDate: new Date("2025-03-20"),
             endDate: new Date("2025-03-26"),
             prizes: [
                 {
                     title: "🥇 Trifecta TEE Track Winner",
                 },
-				{
+                {
                     title: "🏆 Best use of Marlin TEEs",
                 },
             ],
+			links: [
+				{
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/pauldev20/pass-tee-port"
+				},
+				{
+					title: "ETHGlobal",
+					icon: faEthereum,
+					link: "https://ethglobal.com/showcase/pass-tee-port-2qoc1"
+				}
+			]
         },
         {
             title: "ETHGlobal Taipei",
@@ -317,9 +455,22 @@ export const pageData = {
             location: "Taipei, Taiwan",
             locationPoint: [25.0375198, 121.5636796],
             livingLocationPoint: [49.1427, 9.2109],
-            description: "Built a service enabling organizations to reimburse real-world expenses for employees, students, and hackers",
+            description:
+                "Built a service enabling organizations to reimburse real-world expenses for employees, students, and hackers",
             startDate: new Date("2025-04-04"),
             endDate: new Date("2025-04-06"),
+			links: [
+				{
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/pauldev20/touchgrass"
+				},
+				{
+					title: "ETHGlobal",
+					icon: faEthereum,
+					link: "https://ethglobal.com/showcase/touch-grass-q4033"
+				}
+			]
         },
         {
             title: "ETHGlobal Cannes",
@@ -338,6 +489,18 @@ export const pageData = {
                     title: "🥇 ETHGlobal Finalist",
                 },
             ],
+			links: [
+				{
+					title: "GitHub",
+					icon: faGithub,
+					link: "https://github.com/pauldev20/onlydanks"
+				},
+				{
+					title: "ETHGlobal",
+					icon: faEthereum,
+					link: "https://ethglobal.com/showcase/onlydanks-wi77b"
+				}
+			]
         },
     ],
     projectsDescription: "Some mentionable projects of mine",
