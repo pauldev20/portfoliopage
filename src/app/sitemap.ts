03c1d1bccd5d2afea0f1cdd33c4e23a7eb5f9367
Paul Geeser <commit@pauldev.sh>
2025-11-19 00:48:40 -0300

    🔍️ Modified Sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: process.env.NEXT_PUBLIC_APP_URL || "",
+            url: process.env.NEXT_PUBLIC_APP_URL || `https://${process.env.VERCEL_URL}` || "http://localhost:3000",
             lastModified: new Date(),
             changeFrequency: "weekly",
             priority: 1,
