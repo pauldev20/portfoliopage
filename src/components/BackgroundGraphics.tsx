7195d6b6be2df49c66e7c94420544ddcf770719c
Paul Geeser <commit@pauldev.sh>
2025-11-19 12:17:29 -0300

    💄 Improved Background

--- src/components/BackgroundGraphics.tsx
+++ src/components/BackgroundGraphics.tsx
@@ -3,7 +3,7 @@ import { AnimatedGridPattern } from "@/components/ui/animated-grid-pattern";
 export default function BackgroundGraphics() {
     return (
         <div className="fixed inset-0 -z-10 overflow-hidden">
-            <AnimatedGridPattern numSquares={30} maxOpacity={0.1} duration={3} className="opacity-40" />
+            <AnimatedGridPattern numSquares={30} maxOpacity={0.1} duration={3} className="opacity-35" />
 
             <div className="absolute inset-0 overflow-hidden">
                 {/* Top left */}
@@ -11,15 +11,7 @@ export default function BackgroundGraphics() {
 
                 {/* Top right */}
                 <div className="absolute -top-40 -right-40 h-80 w-80 md:h-100 md:w-100 rounded-full bg-purple-400/20 blur-3xl" />
-
-                {/* Bottom left */}
-                {/* <div className="absolute -bottom-40 -left-40 h-80 w-80 rounded-full bg-pink-400/20 blur-3xl" /> */}
-
-                {/* Bottom right */}
-                {/* <div className="absolute -bottom-40 -right-40 h-80 w-80 rounded-full bg-cyan-400/20 blur-3xl" /> */}
             </div>
-
-            {/* <div className="absolute inset-0 bg-gradient-to-b from-transparent via-background/50 to-background" /> */}
         </div>
     );
 }
