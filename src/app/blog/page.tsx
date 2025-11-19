4739bf131e8cb46ca76fe2a76d3de8129ebbe6d5
Paul Geeser <commit@pauldev.sh>
2025-11-19 13:33:18 -0300

    ✨ Added Blog Page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+import UnderConstruction from "@/components/UnderConstruction";
+
+export default function BlogPage() {
+    return (
+		<main className="flex flex-col items-center justify-center max-w-4xl space-y-10 flex-1 w-full">
+			<UnderConstruction />
+		</main>
+	);
+}
