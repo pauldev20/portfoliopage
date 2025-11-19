aa0e9d30cbd60c735de8c5cdcc486a8e32e63f91
Paul Geeser <commit@pauldev.sh>
2025-11-18 21:02:27 -0300

    💄 Added Footer & Background Graphics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Inter as FontSans } from "next/font/google";
 
 import "./globals.css";
 
+import BackgroundGraphics from "@/components/BackgroundGraphics";
+
 import { pageData } from "@/pageData";
 
 /* -------------------------------------------------------------------------- */
@@ -35,17 +37,23 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body
-                className={`${fontSans.variable} min-h-screen bg-background font-sans antialiased flex justify-center mx-auto py-8 sm:py-16 px-6`}
+			<body
+                className={`${fontSans.variable} min-h-screen bg-background font-sans antialiased flex flex-col items-center mx-auto py-8 sm:py-16 px-5`}
             >
                 {children}
+
+				<footer className="py-5 sm:py-0 sm:pt-10 text-center text-sm text-muted-foreground mt-5">
+					Made with ❤️ by <strong>pauldev</strong>
+				</footer>
 
                 {/* ------------------------------- Bottom Blur ------------------------------ */}
                 <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background" />
 
                 <SpeedInsights />
                 <Analytics />
-            </body>
+
+                <BackgroundGraphics />
+			</body>
         </html>
     );
 }
