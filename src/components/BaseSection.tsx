6e66946a8c1594d5ff58f7b4e192d029843d9764
Paul Geeser <commit@pauldev.sh>
2025-11-19 16:29:02 -0300

    🎨 More Code Improvements

--- src/components/BaseSection.tsx
+++ src/components/BaseSection.tsx
@@ -5,11 +5,9 @@ interface BaseSectionProps {
 
 export default function BaseSection({ title, children }: BaseSectionProps) {
     return (
-        <section id={title.toLowerCase().replace(" ", "-")}>
-            <div className="w-full flex min-h-0 flex-col gap-y-2">
-                <h2 className="text-xl sm:text-2xl font-bold">{title}</h2>
-                {children}
-            </div>
+        <section id={title.toLowerCase().replace(" ", "-")} className="w-full min-h-0 space-y-2">
+			<h2 className="text-xl sm:text-2xl font-bold">{title}</h2>
+			{children}
         </section>
     );
 }
