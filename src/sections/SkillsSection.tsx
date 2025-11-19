2d875ae764c4b604bfa744787f0c6cf9b8b92053
Paul Geeser <commit@pauldev.sh>
2025-11-19 12:24:31 -0300

    🎨 Imporved SkillsSection Code

--- src/sections/SkillsSection.tsx
+++ src/sections/SkillsSection.tsx
@@ -11,15 +11,13 @@ interface SkillsSectionProps {
 export default function SkillsSection({ skills, description }: SkillsSectionProps) {
     return (
         <BaseSection title="Skills">
-            <div className="flex flex-col gap-y-2">
-                <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
-                    {description}
-                </Markdown>
-                <div className="flex flex-wrap gap-1 justify-start">
-                    {skills.map((skill) => (
-                        <Badge key={skill}>{skill}</Badge>
-                    ))}
-                </div>
+            <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+                {description}
+            </Markdown>
+            <div className="flex flex-wrap gap-1 justify-start">
+                {skills.map((skill) => (
+                    <Badge key={skill}>{skill}</Badge>
+                ))}
             </div>
         </BaseSection>
     );
